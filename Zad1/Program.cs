ef5ea18d2dfc51790733dbc5c7162902082940e5
1211-2 <1211-2@DESKTOP-PMVUDLD>
2024-10-29 08:39:37 +0200

    Add project files.

---
+++ Zad1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zad1
+{   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadA = new Thread(PrintFromA);
+            Thread threadY = new Thread(PrintFromY);
+            Thread threadChild = new Thread(PrintChild);
+            threadA.Start();
+            threadY.Start();
+            threadA.Join();
+            threadY.Join();
+            threadChild.Start();
+            threadChild.Join();
+        }
+        static void PrintFromA()
+        {
+            char start = 'А';
+            char end = 'Й';
+            for (char c = start; c <= end; c++)
+            {
+                Console.Write(c + " ");
+                Thread.Sleep(1000);
+            }
+        }
+        static void PrintFromY()
+        {
+            char start = 'Я';
+            for (char c = start; c >= 'А'; c--)
+            {
+                Console.Write(c + " ");
+                Thread.Sleep(1500);
+            }
+        }
+        static void PrintChild()
+        {
+            char start = 'А';
+            char end = 'Я';
+            for (char c = start; c <= end; c++)
+            {
+                Console.Write(c + " ");
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
